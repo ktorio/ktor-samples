99e509b29a6fa287881d6ce58d4f382a383a235d
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-03-19 02:01:28 +0100

    Docker sample

---
+++ deployment/docker/src/HelloApplication.kt
@@ -0,0 +1,26 @@
+package io.ktor.samples.hello
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.html.*
+import io.ktor.routing.*
+import kotlinx.html.*
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    routing {
+        get("/") {
+            call.respondHtml {
+                head {
+                    title { +"Ktor: Docker" }
+                }
+                body {
+                    p {
+                        +"Hello from Ktor Netty engine running in Docker sample application"
+                    }
+                }
+            }
+        }
+    }
+}
