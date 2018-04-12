655fee932b63659885710a3bc958b205b87c1c8a
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-04-12 13:30:46 +0200

    Added a test with a conversation between two clients

--- app/chat/test/ChatApplicationTest.kt
+++ app/chat/test/ChatApplicationTest.kt
@@ -23,4 +23,36 @@ class ChatApplicationTest {
             )
         }
     }
+
+    @Test
+    fun testDualConversation() {
+        withTestApplication(Application::main) {
+            val log1 = hashSetOf<String>()
+            val log2 = hashSetOf<String>()
+            handleWebSocketConversation("/ws") { incoming1, outgoing1 ->
+                log1 += (incoming1.receive() as Frame.Text).readText()
+                handleWebSocketConversation("/ws") { incoming2, outgoing2 ->
+                    outgoing1.send(Frame.Text("HELLO"))
+                    outgoing2.send(Frame.Text("HI"))
+                    for (n in 0 until 3) log1 += (incoming1.receive() as Frame.Text).readText()
+                    for (n in 0 until 3) log2 += (incoming2.receive() as Frame.Text).readText()
+                }
+            }
+            assertEquals(
+                setOf(
+                    "[server] Member joined: user1.",
+                    "[server] Member joined: user2.",
+                    "[user1] HELLO",
+                    "[user2] HI"
+                ), log1
+            )
+            assertEquals(
+                setOf(
+                    "[server] Member joined: user2.",
+                    "[user1] HELLO",
+                    "[user2] HI"
+                ), log2
+            )
+        }
+    }
 }
