fc4ef745c34098e48610f322999c87e501fd26bb
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-04-12 16:21:49 +0200

    Initial test for HttpBin application

---
+++ app/httpbin/test/HttpBinApplicationTest.kt
@@ -0,0 +1,33 @@
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.samples.httpbin.*
+import io.ktor.server.testing.*
+import kotlinx.coroutines.experimental.*
+import kotlin.test.*
+
+class HttpBinApplicationTest {
+    @Test
+    fun testRedirect() {
+        testRequest(HttpMethod.Get, "/redirect/2") { assertEquals("/redirect/1", response.headers["Location"]) }
+        testRequest(HttpMethod.Get, "/redirect/1") { assertEquals("/redirect/0", response.headers["Location"]) }
+        testRequest(HttpMethod.Get, "/redirect/0") { assertEquals(null, response.headers["Location"]) }
+    }
+}
+
+private fun testRequest(
+    method: HttpMethod,
+    uri: String,
+    setup: suspend TestApplicationRequest.() -> Unit = {},
+    checks: suspend TestApplicationCall.() -> Unit
+) {
+    httpBinTest {
+        val req = handleRequest(method, uri) { runBlocking { setup() } }
+        checks(req)
+    }
+}
+
+private fun httpBinTest(callback: suspend TestApplicationEngine.() -> Unit): Unit {
+    withTestApplication(Application::main) {
+        runBlocking { callback() }
+    }
+}
