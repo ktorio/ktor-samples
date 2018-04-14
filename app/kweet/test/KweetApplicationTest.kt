a46d2c5bf21cccd4a3712abcd7681035a7832852
Carlos Ballesteros Velasco <soywiz@gmail.com>
2018-04-14 15:31:51 +0200

    Kweet: test root route using mockk

--- app/kweet/test/KweetApplicationTest.kt
+++ app/kweet/test/KweetApplicationTest.kt
@@ -1,61 +1,50 @@
 package io.ktor.samples.kweet
 
+import io.ktor.http.*
 import io.ktor.samples.kweet.dao.*
 import io.ktor.samples.kweet.model.*
 import io.ktor.server.testing.*
+import io.mockk.*
 import org.joda.time.*
-import org.junit.*
+import org.junit.Test
+import kotlin.test.*
 
 class KweetApplicationTest {
-    @Test
-    fun name() {
-        withTestApplication {
+    val dao = mockk<DAOFacade>(relaxed = true)
+    val date = DateTime.parse("2010-01-01T00:00+00:00")
 
+    @Test
+    fun testEmptyHome() = testApp {
+        every { dao.top() } returns listOf()
+        every { dao.latest() } returns listOf()
+
+        this.handleRequest(HttpMethod.Get, "/").apply {
+            assertEquals(200, response.status()?.value)
+            assertTrue(response.content!!.contains("There are no kweets yet"))
         }
     }
 
-    class TestDAOFacade : DAOFacade {
-        override fun init() = Unit
-        override fun countReplies(id: Int): Int {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    @Test
+    fun testHomeWithSomeKweets() = testApp {
+        every { dao.getKweet(1) } returns Kweet(1, "user1", "text1", date, null)
+        every { dao.getKweet(2) } returns Kweet(2, "user2", "text2", date, null)
+        every { dao.top() } returns listOf(1)
+        every { dao.latest() } returns listOf(2)
+
+        this.handleRequest(HttpMethod.Get, "/").apply {
+            assertEquals(200, response.status()?.value)
+            assertFalse(response.content!!.contains("There are no kweets yet"))
+            assertTrue(response.content!!.contains("user1"))
+            assertTrue(response.content!!.contains("user2"))
         }
 
-        override fun createKweet(user: String, text: String, replyTo: Int?, date: DateTime): Int {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
+        verify(exactly = 1) { dao.getKweet(1) }
+        verify(exactly = 1) { dao.getKweet(2) }
+        verify(exactly = 1) { dao.top() }
+        verify(exactly = 1) { dao.latest() }
+    }
 
-        override fun deleteKweet(id: Int) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun getKweet(id: Int): Kweet {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun userKweets(userId: String): List<Int> {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun user(userId: String, hash: String?): User? {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun userByEmail(email: String): User? {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun createUser(user: User) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun top(count: Int): List<Int> {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun latest(count: Int): List<Int> {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        override fun close() = Unit
+    private fun testApp(callback: TestApplicationEngine.() -> Unit) {
+        withTestApplication({ mainWithDependencies(dao) }) { callback() }
     }
 }
