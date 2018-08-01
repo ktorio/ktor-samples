7c25f23a919b43ce8f1cf6d4f27aa76342576afc
soywiz <soywiz@gmail.com>
2018-08-01 12:13:43 +0200

    Added a sample including HttpClient.getAsTempFile

---
+++ other/client-tools/README.md
@@ -0,0 +1,13 @@
+# Client Tools
+
+Sample project for [Ktor](http://ktor.io) showing several useful extension methods not included in Ktor itself.
+
+## Running
+
+Execute this command in the repository's root directory to run this sample:
+
+```bash
+./gradlew :client-tools:run
+```
+
+Note: for this sample to work you need to open a server in your localhost at port 8080.
---
+++ other/client-tools/src/ToolsApp.kt
@@ -0,0 +1,49 @@
+import io.ktor.cio.*
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.features.*
+import io.ktor.client.request.*
+import io.ktor.client.response.*
+import io.ktor.http.*
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.io.*
+import java.io.*
+import java.net.*
+
+fun main(args: Array<String>) {
+    runBlocking {
+        val client = HttpClient(Apache) {
+            install(HttpRedirect) {
+                maxJumps = 20
+            }
+        }
+        client.getAsTempFile("http://127.0.0.1:8080/") { file ->
+            println(file.readBytes().size)
+        }
+    }
+}
+
+data class HttpClientException(val response: HttpResponse) : IOException("HTTP Error ${response.status}")
+
+suspend fun HttpClient.getAsTempFile(url: String, callback: suspend (file: File) -> Unit) {
+    val file = getAsTempFile(url)
+    try {
+        callback(file)
+    } finally {
+        file.delete()
+    }
+}
+
+suspend fun HttpClient.getAsTempFile(url: String): File {
+    val file = File.createTempFile("ktor", "http-client")
+    val call = call {
+        url(URL(url))
+        method = HttpMethod.Get
+    }
+    if (!call.response.status.isSuccess()) {
+        throw HttpClientException(call.response)
+    }
+    call.response.content.copyAndClose(file.writeChannel())
+    return file
+}
