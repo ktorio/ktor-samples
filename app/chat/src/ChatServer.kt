21a6ac5f454d6dfcee8ffe8b3c2ef65f5bbcf1e4
soywiz <soywiz@gmail.com>
2018-09-14 08:37:42 +0200

    Chat documentation: some work on https://github.com/ktorio/ktor/issues/269

--- app/chat/src/ChatServer.kt
+++ app/chat/src/ChatServer.kt
@@ -6,59 +6,123 @@ import java.util.*
 import java.util.concurrent.*
 import java.util.concurrent.atomic.*
 
+/**
+ * Class in charge of the logic of the chat server.
+ * It contains handlers to events and commands to send messages to specific users in the server.
+ */
 class ChatServer {
+    /**
+     * Atomic counter used to get unique user-names based on the maxiumum users the server had.
+     */
     val usersCounter = AtomicInteger()
+
+    /**
+     * A concurrent map associating session IDs to user names.
+     */
     val memberNames = ConcurrentHashMap<String, String>()
+
+    /**
+     * Associates a session-id to a set of websockets.
+     * Since a browser is able to open several tabs and windows with the same cookies and thus the same session.
+     * There might be several opened sockets for the same client.
+     */
     val members = ConcurrentHashMap<String, MutableList<WebSocketSession>>()
+
+    /**
+     * A list of the lastest messages sent to the server, so new members can have a bit context of what
+     * other people was talking about before joining.
+     */
     val lastMessages = LinkedList<String>()
 
+    /**
+     * Handles that a member identified with a session id and a socket joined.
+     */
     suspend fun memberJoin(member: String, socket: WebSocketSession) {
+        // Checks if this user is already registered in the server and gives him/her a temporal name if required.
         val name = memberNames.computeIfAbsent(member) { "user${usersCounter.incrementAndGet()}" }
+
+        // Associates this socket to the member id.
+        // Since iteration is likely to happen more frequently than adding new items,
+        // we use a `CopyOnWriteArrayList`.
+        // We could also control how many sockets we would allow per client here before appending it.
+        // But since this is a sample we are not doing it.
         val list = members.computeIfAbsent(member) { CopyOnWriteArrayList<WebSocketSession>() }
         list.add(socket)
 
+        // Only when joining the first socket for a member notifies the rest of the users.
         if (list.size == 1) {
             broadcast("server", "Member joined: $name.")
         }
 
+        // Sends the user the latest messages from this server to let the member have a bit context.
         val messages = synchronized(lastMessages) { lastMessages.toList() }
         for (message in messages) {
             socket.send(Frame.Text(message))
         }
     }
 
+    /**
+     * Handles a [member] idenitified by its session id renaming [to] a specific name.
+     */
     suspend fun memberRenamed(member: String, to: String) {
+        // Re-sets the member name.
         val oldName = memberNames.put(member, to) ?: member
+        // Notifies everyone in the server about this change.
         broadcast("server", "Member renamed from $oldName to $to")
     }
 
+    /**
+     * Handles that a [member] with a specific [socket] left the server.
+     */
     suspend fun memberLeft(member: String, socket: WebSocketSession) {
+        // Removes the socket connection for this member
         val connections = members[member]
         connections?.remove(socket)
 
+        // If no more sockets are connected for this member, let's remove it from the server
+        // and notify the rest of the users about this event.
         if (connections != null && connections.isEmpty()) {
-            val name = memberNames[member] ?: member
+            val name = memberNames.remove(member) ?: member
             broadcast("server", "Member left: $name.")
         }
     }
 
+    /**
+     * Handles the 'who' command by sending the member a list of all all members names in the server.
+     */
     suspend fun who(sender: String) {
         members[sender]?.send(Frame.Text(memberNames.values.joinToString(prefix = "[server::who] ")))
     }
 
+    /**
+     * Handles the 'help' command by sending the member a list of available commands.
+     */
     suspend fun help(sender: String) {
         members[sender]?.send(Frame.Text("[server::help] Possible commands are: /user, /help and /who"))
     }
 
+    /**
+     * Handles sending to a [recipient] from a [sender] a [message].
+     *
+     * Both [recipient] and [sender] are identified by its session-id.
+     */
     suspend fun sendTo(recipient: String, sender: String, message: String) {
         members[recipient]?.send(Frame.Text("[$sender] $message"))
     }
 
+    /**
+     * Handles a [message] sent from a [sender] by notifying the rest of the users.
+     */
     suspend fun message(sender: String, message: String) {
+        // Pre-format the message to be send, to prevent doing it for all the users or connected sockets.
         val name = memberNames[sender] ?: sender
         val formatted = "[$name] $message"
 
+        // Sends this pre-formatted message to all the members in the server.
         broadcast(formatted)
+
+        // Appends the message to the list of [lastMessages] and caps that collection to 100 items to prevent
+        // growing too much.
         synchronized(lastMessages) {
             lastMessages.add(formatted)
             if (lastMessages.size > 100) {
@@ -67,17 +131,26 @@ class ChatServer {
         }
     }
 
+    /**
+     * Sends a [message] to all the members in the server, including all the connections per member.
+     */
     private suspend fun broadcast(message: String) {
         members.values.forEach { socket ->
             socket.send(Frame.Text(message))
         }
     }
 
+    /**
+     * Sends a [message] coming from a [sender] to all the members in the server, including all the connections per member.
+     */
     private suspend fun broadcast(sender: String, message: String) {
         val name = memberNames[sender] ?: sender
         broadcast("[$name] $message")
     }
 
+    /**
+     * Sends a [message] to a list of [this] [WebSocketSession].
+     */
     suspend fun List<WebSocketSession>.send(frame: Frame) {
         forEach {
             try {
--- app/chat/test/ChatApplicationTest.kt
+++ app/chat/test/ChatApplicationTest.kt
@@ -4,17 +4,34 @@ import io.ktor.samples.chat.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 
+/**
+ * Tests the [ChatApplication].
+ */
 class ChatApplicationTest {
+    /**
+     * This is an integration test that verifies the behaviour of a simple conversation with an empty server.
+     */
     @Test
     fun testSimpleConversation() {
+        // First we create a [TestApplicationEngine] that includes the module [Application.main],
+        // this executes that function and thus installs all the features and routes to this test application.
         withTestApplication(Application::main) {
+            // Keeps a log array that will hold all the events we want to check later at once.
             val log = arrayListOf<String>()
+
+            // We perform a test websocket connection to this route. Effectively acting as a client.
+            // The [incoming] parameter allows to receive frames, while the [outgoing] allows to send frames to the server.
             handleWebSocketConversation("/ws") { incoming, outgoing ->
+                // Send a HELLO message
                 outgoing.send(Frame.Text("HELLO"))
+
+                // We then receive two messages (the message notifying that the member joined, and the message we sent echoed to us)
                 for (n in 0 until 2) {
                     log += (incoming.receive() as Frame.Text).readText()
                 }
             }
+
+            // Verify the received messages.
             assertEquals(
                 listOf(
                     "[server] Member joined: user1.",
@@ -24,20 +41,40 @@ class ChatApplicationTest {
         }
     }
 
+    /**
+     * This is an integration test that verifies the behaviour with two connected clients.
+     *
+     * NOTE: to prevent repeating, check the previous test for detailed explanation of what does what.
+     */
     @Test
     fun testDualConversation() {
+        // Creates the [TestApplicationEngine] with the [Application::main] module. Check the previous test for more details.
         withTestApplication(Application::main) {
+            // Sets to hold the messages from each children.
+            // Since this is multithreaded and socket-related.
+            // The order might change in each run, so we use a Set instead of a List to check that the messages
+            // are received without having into account exact the order.
             val log1 = hashSetOf<String>()
             val log2 = hashSetOf<String>()
+
+            // Perform a test connection to this route (client1): incoming1, outgoing1
             handleWebSocketConversation("/ws") { incoming1, outgoing1 ->
+                // We log the `Member joined` for this user.
                 log1 += (incoming1.receive() as Frame.Text).readText()
+
+                // Perform a test connection to this route (client2): incoming2, outgoing2
                 handleWebSocketConversation("/ws") { incoming2, outgoing2 ->
-                    outgoing1.send(Frame.Text("HELLO"))
-                    outgoing2.send(Frame.Text("HI"))
+                    // A Member joined: user2 happens here
+                    outgoing1.send(Frame.Text("HELLO")) // Client1 says HELLO
+                    outgoing2.send(Frame.Text("HI")) // Client2 says HI
+
+                    // Both clients now have three messages to read (Member joined + HELLO + HI)
                     for (n in 0 until 3) log1 += (incoming1.receive() as Frame.Text).readText()
                     for (n in 0 until 3) log2 += (incoming2.receive() as Frame.Text).readText()
                 }
             }
+
+            // Checks what USER1 received
             assertEquals(
                 setOf(
                     "[server] Member joined: user1.",
@@ -46,6 +83,8 @@ class ChatApplicationTest {
                     "[user2] HI"
                 ), log1
             )
+
+            // Checks what USER2 received
             assertEquals(
                 setOf(
                     "[server] Member joined: user2.",
