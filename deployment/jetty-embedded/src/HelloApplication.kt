409ca00fe07e67d94a319771ef743aba2f0339c7
soywiz <soywiz@gmail.com>
2018-09-15 21:43:25 +0200

    API docs for remaining deployment projects

--- deployment/jetty-embedded/src/HelloApplication.kt
+++ deployment/jetty-embedded/src/HelloApplication.kt
@@ -8,10 +8,25 @@ import io.ktor.server.engine.*
 import io.ktor.server.jetty.*
 import kotlinx.html.*
 
+/**
+ * Entry Point of the application. This function is referenced in the
+ * resources/application.conf file inside the ktor.application.modules.
+ *
+ * For more information about this file: https://ktor.io/servers/configuration.html#hocon-file
+ */
 fun Application.main() {
+    // This adds automatically Date and Server headers to each response, and would allow you to configure
+    // additional headers served to each response.
     install(DefaultHeaders)
+    // This uses use the logger to log every call (request/response)
     install(CallLogging)
+
+    // Registers routes
     routing {
+        // For the root / route, we respond with an Html.
+        // The `respondHtml` extension method is available at the `ktor-html-builder` artifact.
+        // It provides a DSL for building HTML to a Writer, potentially in a chunked way.
+        // More information about this DSL: https://github.com/Kotlin/kotlinx.html
         get("/") {
             call.respondHtml {
                 head {
--- deployment/tomcat-war/src/HelloApplication.kt
+++ deployment/tomcat-war/src/HelloApplication.kt
@@ -5,8 +5,19 @@ import io.ktor.html.*
 import io.ktor.routing.*
 import kotlinx.html.*
 
+/**
+ * Entry Point of the application. This function is referenced in the
+ * resources/application.conf file inside the ktor.application.modules.
+ *
+ * For more information about this file: https://ktor.io/servers/configuration.html#hocon-file
+ */
 fun Application.main() {
+    // Registers routes
     routing {
+        // For the root / route, we respond with an Html.
+        // The `respondHtml` extension method is available at the `ktor-html-builder` artifact.
+        // It provides a DSL for building HTML to a Writer, potentially in a chunked way.
+        // More information about this DSL: https://github.com/Kotlin/kotlinx.html
         get("/") {
             call.respondHtml {
                 head {
