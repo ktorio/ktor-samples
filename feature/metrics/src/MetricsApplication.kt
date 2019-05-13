6a91ef60e85603732ee546e0ac0d89e78f7ea74f
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2019-05-13 17:15:54 +0300

    Update dropwizard metrics example
    Signed-off-by: Sergey Mashkov <sergey.mashkov@jetbrains.com>

--- feature/metrics/src/MetricsApplication.kt
+++ feature/metrics/src/MetricsApplication.kt
@@ -4,16 +4,14 @@ import com.codahale.metrics.*
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.html.*
-import io.ktor.http.*
-import io.ktor.metrics.*
-import io.ktor.response.*
+import io.ktor.metrics.dropwizard.*
 import io.ktor.routing.*
 import kotlinx.html.*
 import java.util.concurrent.*
 
 fun Application.main() {
     install(DefaultHeaders)
-    install(Metrics) {
+    install(DropwizardMetrics) {
         val reporter = Slf4jReporter.forRegistry(registry)
                 .outputTo(log)
                 .convertRatesTo(TimeUnit.SECONDS)
