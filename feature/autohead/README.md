f1996cc45420285931d3c2c87b35d1003004298f
hadihariri <mail@hadihariri.com>
2020-08-14 14:04:14 +0200

    Added sample for AutoHeadResponse

---
+++ feature/autohead/README.md
@@ -0,0 +1,13 @@
+# AutoHeadResponse
+
+Sample project for [Ktor](https://ktor.io) showing AutoHeadResponse functionality.
+
+## Running
+
+Execute this command in the repository's root directory to run this sample:
+
+```bash
+./gradlew :autohead:run
+```
+ 
+And make a HEAD request to [http://localhost:8080/home] to see the response.
---
+++ feature/autohead/src/AutoHead.kt
@@ -0,0 +1,20 @@
+package io.ktor.samples.autohead
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.html.*
+import io.ktor.http.content.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import kotlinx.html.*
+import java.io.*
+
+fun Application.main() {
+    install(AutoHeadResponse)
+    routing {
+        get("/home") {
+            call.respondText("This is a response to a GET, but HEAD also works")
+        }
+    }
+}
+
