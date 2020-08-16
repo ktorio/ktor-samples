83042b8d3ec13602a804a419dfc7328b3106589b
hadihariri <mail@hadihariri.com>
2020-08-16 09:34:53 +0200

    Switched to Kotlin 1.4.0 and Gradle 6.6

--- feature/autohead/src/AutoHead.kt
+++ feature/autohead/src/AutoHead.kt
@@ -2,12 +2,8 @@ package io.ktor.samples.autohead
 
 import io.ktor.application.*
 import io.ktor.features.*
-import io.ktor.html.*
-import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import kotlinx.html.*
-import java.io.*
 
 fun Application.main() {
     install(AutoHeadResponse)
