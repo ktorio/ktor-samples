074499fc637b55b6bb785d5d55f56c53b2a45d0d
hhariri <mail@hadihariri.com>
2020-09-22 10:39:01 +0200

    Remove unwanted dependencies and repositories. Clean-up code.

--- generic/samples/location-header/src/LocationHeaderApplication.kt
+++ generic/samples/location-header/src/LocationHeaderApplication.kt
@@ -12,10 +12,9 @@ import io.ktor.server.netty.*
  * Main entrypoint of the executable that starts a Netty webserver at port 8080
  * and registers the [module].
  *
- * This is a hello-world application, while the important part is that the build.gradle
- * includes all the available artifacts and serves to use as module for a scratch or to autocomplete APIs.
  */
-fun main(args: Array<String>) {
+
+fun main() {
     embeddedServer(Netty, port = 8080) { module()}.start(wait = true)
 }
 
@@ -25,11 +24,13 @@ fun main(args: Array<String>) {
 fun Application.module() {
     routing {
         post("/manual") {
+            // AF4GH is a sample code for demo purposes
             call.response.header("Location", "/manual/AF4GH")
             call.response.status(HttpStatusCode.Created)
             call.respondText("Manually setting the location header")
         }
         post("/extension") {
+            // AF4GH is a sample code for demo purposes
             call.response.created("AF4GH")
             call.respondText("Extension setting the location header")
         }
