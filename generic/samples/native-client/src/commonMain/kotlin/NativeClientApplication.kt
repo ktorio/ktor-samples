8846158930db7ce0dc91b9d96835c286b50020f8
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2020-10-21 12:27:13 +0300

    KTOR-942 Fix "event loop was shut down" error Fix non-zero exit status because of memory leaks

--- generic/samples/native-client/src/commonMain/kotlin/NativeClientApplication.kt
+++ generic/samples/native-client/src/commonMain/kotlin/NativeClientApplication.kt
@@ -3,7 +3,17 @@ import io.ktor.client.engine.*
 import io.ktor.client.request.*
 
 suspend fun runClient(engine: HttpClientEngine) {
+    // To prevent non-zero exit status because of detected memory leaks
+    kotlin.native.Platform.isMemoryLeakCheckerActive = false
+
     val client = HttpClient(engine)
-    val response = client.get<String>("https://httpbin.org/get")
-    print(response)
+    try {
+        val response = client.get<String>("http://example.com")
+        print(response)
+    }
+    finally {
+        // To prevent "IllegalStateException: Cannot execute task because event loop was shut down"
+        client.close()
+        engine.close()
+    }
 }
