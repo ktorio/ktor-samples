03de84e576498859af85f0d7d76767b05259d5ae
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2020-10-26 13:02:12 +0300

    KTOR-942 Use curl engine instead of cio because of Windows target

--- generic/samples/native-client/src/nativeMain/kotlin/app.kt
+++ generic/samples/native-client/src/nativeMain/kotlin/app.kt
@@ -1,4 +1,4 @@
-import io.ktor.client.engine.cio.*
+import io.ktor.client.engine.curl.Curl
 import io.ktor.util.*
 import kotlinx.coroutines.runBlocking
 
@@ -7,5 +7,5 @@ fun main() = runBlocking {
     // To prevent non-zero exit status because of detected memory leaks
     Platform.isMemoryLeakCheckerActive = false
 
-    runClient(CIO.create())
+    runClient(Curl.create())
 }
