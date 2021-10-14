66613f8a97384d2f3b0a4225e658aeb219443217
Hadi Hariri <mail@hadihariri.com>
2021-10-14 07:05:53 +0200

    Migrate native-client to 2.0

--- generic/samples/native-client/src/commonMain/kotlin/runner.kt
+++ generic/samples/native-client/src/commonMain/kotlin/runner.kt
@@ -1,12 +1,13 @@
 import io.ktor.client.*
 import io.ktor.client.engine.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 
 suspend fun runClient(engine: HttpClientEngine) {
     val client = HttpClient(engine)
     try {
-        val response = client.get<String>("http://example.com")
-        print(response)
+        val response = client.get("http://example.com")
+        print(response.bodyAsText())
     } finally {
         // To prevent IllegalStateException https://youtrack.jetbrains.com/issue/KTOR-1071
         client.close()
