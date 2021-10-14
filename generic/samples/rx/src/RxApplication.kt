d8fe044e60f9039568c10f90614521fefd354cbc
Hadi Hariri <mail@hadihariri.com>
2021-10-14 07:59:02 +0200

    Migrate rx to 2.0

--- generic/samples/rx/src/RxApplication.kt
+++ generic/samples/rx/src/RxApplication.kt
@@ -1,9 +1,9 @@
 package io.ktor.samples.rx
 
-import io.ktor.application.*
+import io.ktor.server.application.*
 import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.reactivex.*
