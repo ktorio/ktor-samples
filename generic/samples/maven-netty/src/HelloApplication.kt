b8bd9b2283ccaf7b217e143e1d947d7e94cbe1d9
Hadi Hariri <mail@hadihariri.com>
2021-10-14 09:37:53 +0200

    Migrate maven-netty to 2.0

--- generic/samples/maven-netty/src/HelloApplication.kt
+++ generic/samples/maven-netty/src/HelloApplication.kt
@@ -1,9 +1,9 @@
 package io.ktor.samples.hello
 
-import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.html.*
-import io.ktor.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.*
+import io.ktor.server.html.*
+import io.ktor.server.routing.*
 import kotlinx.html.*
 
 fun Application.main() {
