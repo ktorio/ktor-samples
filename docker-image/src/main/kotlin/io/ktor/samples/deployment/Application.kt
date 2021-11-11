2f94419b61372ec2435eeb921002f63aeccc65f7
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2021-11-11 11:35:43 +0100

    KTOR-3396 Add sample of building docker image

---
+++ docker-image/src/main/kotlin/io/ktor/samples/deployment/Application.kt
@@ -0,0 +1,12 @@
+package io.ktor.samples.deployment
+
+import io.ktor.samples.deployment.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
---
+++ docker-image/src/test/kotlin/io/ktor/samples/deployment/ApplicationTest.kt
@@ -0,0 +1,12 @@
+package io.ktor.samples.deployment
+
+import io.ktor.http.*
+import io.ktor.samples.deployment.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest
