170b95046d6957be30d6d115f610e15f5bd9a49a
Rustam Musin <rustam.musin@jetbrains.com>
2022-05-24 15:31:26 +0200

    Make use of GraalVM Gradle Plugin

--- graalvm/build.gradle.kts
+++ graalvm/build.gradle.kts
@@ -2,13 +2,13 @@ plugins {
     application
     kotlin("jvm") version "1.6.21"
     id("com.github.johnrengelman.shadow") version "7.1.1"
+    id("org.graalvm.buildtools.native") version "0.9.11"
 }
 
 group = "io.ktor"
 version = "0.0.1"
 application {
-    mainClass.set("io.ktor.ApplicationKt")
-    mainClassName = "io.ktor.ApplicationKt"
+    mainClass.set("io.ktorgraal.ApplicationKt")
 }
 
 repositories {
@@ -21,4 +21,19 @@ dependencies {
     implementation("io.ktor:ktor-server-cio-jvm:2.0.1")
 }
 
+graalvmNative {
+    binaries {
+        named("main") {
+            fallback.set(false)
+            verbose.set(true)
 
+            buildArgs.add("--initialize-at-build-time=io.ktor,kotlin")
+
+            buildArgs.add("-H:+InstallExitHandlers")
+            buildArgs.add("-H:+ReportUnsupportedElementsAtRuntime")
+            buildArgs.add("-H:+ReportExceptionStackTraces")
+
+            imageName.set("graal-server")
+        }
+    }
+}
