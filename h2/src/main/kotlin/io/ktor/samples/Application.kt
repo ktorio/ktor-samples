162be1a3e98b79375aa882501ba7b1e0ea938996
leonid.stashevsky <leonid.stashevsky@gmail.com>
2022-06-03 09:44:05 +0200

    KTOR-4362 Add sample with H2 database

---
+++ h2/src/main/kotlin/io/ktor/samples/Application.kt
@@ -0,0 +1,12 @@
+package io.ktor.samples
+
+import io.ktor.samples.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+    configureRouting()
+}
---
+++ h2/src/test/kotlin/io/ktor/samples/ApplicationTest.kt
@@ -0,0 +1,13 @@
+package io.ktor.samples
+
+import io.ktor.http.*
+import io.ktor.samples.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest {
+}
