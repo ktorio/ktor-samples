8d76715aee506988327fd3f8c9455f1b2a0dd76a
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-10-27 12:14:05 +0400

    KTOR-4979 deprecated appendExpired

--- httpbin/src/HttpBinApplication.kt
+++ httpbin/src/HttpBinApplication.kt
@@ -6,10 +6,10 @@ import io.ktor.content.TextContent
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.serialization.gson.*
-import io.ktor.server.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.html.*
+import io.ktor.server.http.content.*
 import io.ktor.server.plugins.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.callloging.*
@@ -24,6 +24,7 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.*
+import io.ktor.util.date.*
 import io.ktor.util.logging.*
 import kotlinx.coroutines.*
 import java.io.*
@@ -316,7 +317,7 @@ fun Application.main() {
             val params = call.request.queryParameters.names()
             val rawCookies = call.request.cookies.rawCookies
             for (name in params) {
-                call.response.cookies.appendExpired(name, path = "/")
+                call.response.cookies.append(name, "", path = "/", expires = GMTDate.START)
             }
             call.sendHttpBinResponse {
                 clear()
