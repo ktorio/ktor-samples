721530fa2ad4227382fdb351d8e4365e94666f09
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2022-10-28 20:00:04 +0300

    Update Kotlin to 1.7.20

--- graalvm/build.gradle.kts
+++ graalvm/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     application
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
     id("org.graalvm.buildtools.native") version "0.9.11"
 }
 
--- version-diff/build.gradle.kts
+++ version-diff/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
-    kotlin("plugin.serialization") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.20"
     application
 }
 
