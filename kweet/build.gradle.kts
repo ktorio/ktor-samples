ae9c35a12ba4113b501ac4dee14096bb6db2c51e
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2023-02-09 17:50:52 +0200

    KTOR-5512 Remove specific versions of kweet sample

--- kweet/build.gradle.kts
+++ kweet/build.gradle.kts
@@ -1,4 +1,3 @@
-val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
@@ -18,13 +17,13 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-freemarker:$ktor_version")
-    implementation("io.ktor:ktor-server-resources:$ktor_version")
-    implementation("io.ktor:ktor-server-sessions:$ktor_version")
-    implementation("io.ktor:ktor-server-conditional-headers:$ktor_version")
-    implementation("io.ktor:ktor-server-default-headers:$ktor_version")
-    implementation("io.ktor:ktor-server-partial-content:$ktor_version")
-    implementation("io.ktor:ktor-server-call-logging:$ktor_version")
+    implementation("io.ktor:ktor-server-freemarker")
+    implementation("io.ktor:ktor-server-resources")
+    implementation("io.ktor:ktor-server-sessions")
+    implementation("io.ktor:ktor-server-conditional-headers")
+    implementation("io.ktor:ktor-server-default-headers")
+    implementation("io.ktor:ktor-server-partial-content")
+    implementation("io.ktor:ktor-server-call-logging")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("com.h2database:h2:2.1.214")
     implementation("com.mchange:c3p0:0.9.5.5")
@@ -35,8 +34,8 @@ dependencies {
     implementation("joda-time:joda-time:2.11.2")
     implementation("org.freemarker:freemarker:2.3.31")
     implementation("org.ehcache:ehcache:3.9.7")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm")
     testImplementation("io.mockk:mockk:1.13.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
 }
