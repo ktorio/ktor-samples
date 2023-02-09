751f2896e33345d678b085fc3f5edf1da8dae587
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2023-02-09 16:54:17 +0200

    KTOR-5512 Remove specific versions from filelisting sample

--- filelisting/build.gradle.kts
+++ filelisting/build.gradle.kts
@@ -1,4 +1,3 @@
-val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
@@ -16,12 +15,12 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-default-headers:$ktor_version")
-    implementation("io.ktor:ktor-server-html-builder:$ktor_version")
-    implementation("io.ktor:ktor-server-call-logging:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-default-headers")
+    implementation("io.ktor:ktor-server-html-builder")
+    implementation("io.ktor:ktor-server-call-logging")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
