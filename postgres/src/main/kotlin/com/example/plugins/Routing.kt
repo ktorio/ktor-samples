43ebe7896fc28beb954fb2a953594700a25f25d5
Violetta Goncharova <40871415+VGoncharova@users.noreply.github.com>
2023-02-23 23:31:17 +0200

    KTOR-3419 Add sample with Postgres JDBC

--- postgres/src/main/kotlin/com/example/plugins/Routing.kt
+++ postgres/src/main/kotlin/com/example/plugins/Routing.kt
@@ -70,7 +70,7 @@ fun Application.configureRouting() {
 fun Application.connectToPostgres(embedded: Boolean): Connection {
     Class.forName("org.postgresql.Driver")
     if (embedded) {
-        return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
+        return DriverManager.getConnection("jdbc:postgresql://localhost/test;DB_CLOSE_DELAY=-1", "root", "")
     } else {
         val url = environment.config.property("postgres.url").getString()
         val user = environment.config.property("postgres.user").getString()
