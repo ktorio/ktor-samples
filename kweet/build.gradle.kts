ed96ea52c9d688397d0db438f6f40d0af12ebef4
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2023-04-19 16:13:34 +0300

    KTOR-5800 Bump Ktor to 2.3.0

--- kweet/build.gradle.kts
+++ kweet/build.gradle.kts
@@ -4,7 +4,7 @@ val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.0"
-    id("io.ktor.plugin") version "2.2.4"
+    id("io.ktor.plugin") version "2.3.0"
     kotlin("plugin.serialization") version "1.8.0"
 }
 
