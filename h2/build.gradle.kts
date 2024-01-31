bff52b0d33440b324c77f9939de8b804e07b8cb9
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2024-01-31 17:51:02 +0200

    Bump Ktor to 2.3.8

--- h2/build.gradle.kts
+++ h2/build.gradle.kts
@@ -4,7 +4,7 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.9.21"
-    id("io.ktor.plugin") version "2.3.7"
+    id("io.ktor.plugin") version "2.3.8"
 }
 
 group = "io.ktor.samples"
