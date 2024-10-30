a8448cf2661a98669ee9a839038c81146f6c6e99
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2024-10-30 10:23:21 +0200

    Bump version of Ktor to 3.0.1

--- h2/build.gradle.kts
+++ h2/build.gradle.kts
@@ -4,7 +4,7 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm") version "2.0.20"
-    id("io.ktor.plugin") version "3.0.0"
+    id("io.ktor.plugin") version "3.0.1"
 }
 
 group = "io.ktor.samples"
--- opentelemetry/server/build.gradle.kts
+++ opentelemetry/server/build.gradle.kts
@@ -5,7 +5,7 @@ val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.20"
-    id("io.ktor.plugin") version "3.0.0"
+    id("io.ktor.plugin") version "3.0.1"
     id("application")
 }
 
