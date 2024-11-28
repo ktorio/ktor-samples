6aa340f3b53a56d2fd99b6a822140c086ce81fa0
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-11-28 21:38:57 +0000

    Split out :di-kodein-advanced from :di-kodein

---
+++ di-kodein-advanced/README.md
@@ -0,0 +1,13 @@
+# Kodein-DI
+
+A sample project for [Ktor](https://ktor.io) showing how to use [Kodein DI](https://kodein.org/Kodein-DI/) with Ktor.
+
+## Running
+
+Execute this command to run the sample:
+
+```bash
+./gradlew run
+```
+
+Then, navigate to [http://localhost:8080/users](http://localhost:8080/users) to see the sample home page.
--- di-kodein-advanced/build.gradle.kts
+++ di-kodein-advanced/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 application {
-    mainClass.set("io.ktor.samples.kodein.KodeinSimpleApplicationKt")
+    mainClass.set("io.ktor.samples.kodein.KodeinAdvancedApplicationKt")
 }
 
 repositories {
@@ -18,6 +18,9 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-resources")
+    implementation("io.ktor:ktor-server-default-headers")
+    implementation("io.ktor:ktor-server-html-builder")
     implementation("org.kodein.di:kodein-di-jvm:7.17.0")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
