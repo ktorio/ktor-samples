ccea8687b34a94023358060ea3ddf19696a6645f
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2025-06-13 14:26:44 +0300

    Bump Ktor to 3.2.0 (#223)
    * Bump Ktor to 3.2.0
    * Apply workaround for https://youtrack.jetbrains.com/issue/KTOR-8583

--- graalvm/build.gradle.kts
+++ graalvm/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "2.1.20"
-    id("io.ktor.plugin") version "3.1.3"
+    id("io.ktor.plugin") version "3.2.0"
     id("org.graalvm.buildtools.native") version "0.10.6"
     kotlin("plugin.serialization") version "2.1.0"
 }
--- opentelemetry/shared/build.gradle.kts
+++ opentelemetry/shared/build.gradle.kts
@@ -5,7 +5,7 @@ val opentelemetry_sdk_extension_autoconfigure_version: String by project
 
 plugins {
     kotlin("jvm") version "2.1.20"
-    id("io.ktor.plugin") version "3.1.3"
+    id("io.ktor.plugin") version "3.2.0"
 }
 
 dependencies {
