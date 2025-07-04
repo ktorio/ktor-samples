2f81fba15c82c022b2fe8bcd90f8877f72ef255e
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2025-07-04 10:55:24 +0300

    Bump Ktor to 3.2.1

--- sse/build.gradle.kts
+++ sse/build.gradle.kts
@@ -3,7 +3,7 @@ val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "2.1.20"
-    id("io.ktor.plugin") version "3.2.0"
+    id("io.ktor.plugin") version "3.2.1"
 }
 
 application {
