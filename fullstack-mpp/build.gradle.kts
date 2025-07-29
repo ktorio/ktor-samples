dc203ad2a14f076977784467b7b1d65bd2436db4
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2025-07-29 16:55:37 +0300

    Bump Ktor to 3.2.3 (#228)

--- fullstack-mpp/build.gradle.kts
+++ fullstack-mpp/build.gradle.kts
@@ -24,7 +24,7 @@ kotlin {
 
     sourceSets.forEach {
         it.dependencies {
-            implementation(project.dependencies.enforcedPlatform("io.ktor:ktor-bom:3.2.2"))
+            implementation(project.dependencies.enforcedPlatform("io.ktor:ktor-bom:3.2.3"))
         }
     }
 
