ed864337e4133ac040048461ea4c70948d2162e7
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2025-09-15 16:17:37 +0200

    Add OpenAPI sample

---
+++ openapi/build.gradle.kts
@@ -0,0 +1,42 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.2.20"
+    id("io.ktor.plugin") version "3.3.0"
+}
+
+application {
+    mainClass.set("io.ktor.samples.openapi.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+ktor {
+    @OptIn(io.ktor.plugin.OpenApiPreview::class)
+    openApi {
+        title = "OpenAPI example"
+        version = "2.1"
+        summary = "This is a sample API"
+    }
+}
+
+// Builds OpenAPI specification automatically
+tasks.processResources {
+    dependsOn("buildOpenApi")
+}
+
+dependencies {
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.openapi)
+    implementation(ktorLibs.server.contentNegotiation)
+    implementation(ktorLibs.serialization.kotlinx.json)
+    implementation(ktorLibs.server.auth)
+    implementation(ktorLibs.client.core)
+    implementation(ktorLibs.client.apache)
+    implementation(ktorLibs.server.netty)
+    implementation(ktorLibs.server.config.yaml)
+    implementation("ch.qos.logback:logback-classic:1.3.14")
+}
---
+++ openapi/src/main/kotlin/Serialization.kt
@@ -0,0 +1,11 @@
+package io.ktor.samples.openapi
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+}
