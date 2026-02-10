5428e10e76683623d68255dca2c34039f0bb6937
Aleksei.Tirman <aleksei.tirman@jetbrains.com>
2026-02-10 12:49:21 +0200

    Add images docs

--- httpbin/src/main/kotlin/io/ktor/samples/httpbin/images.kt
+++ httpbin/src/main/kotlin/io/ktor/samples/httpbin/images.kt
@@ -1,5 +1,6 @@
 package io.ktor.samples.httpbin
 
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.request.accept
 import io.ktor.server.response.respond
@@ -47,26 +48,66 @@ fun Route.images() {
         }
     }.describe {
         tag("Images")
+
+        summary = "Returns a simple image of the type suggest by the Accept header."
+        responses {
+            HttpStatusCode.OK {
+                description = "An image."
+                ContentType.Image.WEBP()
+                ContentType.Image.SVG()
+                ContentType.Image.JPEG()
+                ContentType.Image.PNG()
+                ContentType.Image.Any()
+            }
+        }
     }
 
     get("/image/jpeg") {
         call.respondResource("sample.jpg")
     }.describe {
         tag("Images")
+        summary = "Returns a simple JPEG image."
+        responses {
+            HttpStatusCode.OK {
+                description = "A JPEG image."
+                ContentType.Image.JPEG()
+            }
+        }
     }
     get("/image/png") {
         call.respondResource("sample.png")
     }.describe {
         tag("Images")
+        summary = "Returns a simple PNG image."
+        responses {
+            HttpStatusCode.OK {
+                description = "A PNG image."
+                ContentType.Image.PNG()
+            }
+        }
     }
     get("/image/svg") {
         call.respondResource("sample.svg")
     }.describe {
         tag("Images")
+        summary = "Returns a simple SVG image."
+        responses {
+            HttpStatusCode.OK {
+                description = "An SVG image."
+                ContentType.Image.SVG()
+            }
+        }
     }
     get("/image/webp") {
         call.respondResource("sample.webp")
     }.describe {
         tag("Images")
+        summary = "Returns a simple WEBP image."
+        responses {
+            HttpStatusCode.OK {
+                description = "A WEBP image."
+                ContentType.Image.WEBP()
+            }
+        }
     }
 }
